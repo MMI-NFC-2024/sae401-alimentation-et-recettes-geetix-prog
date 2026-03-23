2676e89b17995344a44c44ebf9360e85c7bcd745
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2026-03-23 16:13:07 +0100

    fix:SEO complet

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://sae401dev.mathis-guellati.fr',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/admin') && !page.includes('/apis'),
+    }),
+  ],
   security: {
     checkOrigin: false
   },
