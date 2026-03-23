e75afacc0aef66b852c57353f96f96ac4a68dc00
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2026-03-23 10:37:04 +0100

    changePbUrl

--- scripts/typegen.sh
+++ scripts/typegen.sh
@@ -1,2 +1,2 @@
 source .env
-bunx pocketbase-typegen --url https://nura.mathis-guellati.fr --email "$PB_EMAIL" --password "$PB_PASSWORD" --out ./src/pocketbase-types.ts
+bunx pocketbase-typegen --url https://sae401dev.mathis-guellati.fr --email "$PB_EMAIL" --password "$PB_PASSWORD" --out ./src/pocketbase-types.ts
